912bddd795271ab4b624318e52d7a4ebf2200096
Satheeshkumar Manoharan <infosatheesh2020@gmail.com>
2018-06-11 15:14:21 +0530

    Added migration script to migrate managed disk across subscription in same region

---
+++ 01.VM_Migrate_region/MigrateManagedVmDiskAcrossSubscription.ps1
@@ -0,0 +1,32 @@
+﻿#*****************************************************************************************************#
+#                                                                                                     #
+#  This script migrates Azure Virtual Machines managed disks from one subscription to another         #
+#  subscription in the same geography. User should have access to both subscripotions                 #
+#                                                                                                     #
+#  Pass required values as parameters, otherwise it will prompt                                       #
+#                                                                                                     #
+# © Satheeshkumar Manoharan, Anyone can use without any warranty in non-production environment        #
+#                                                                                                     #
+#*****************************************************************************************************#
+
+param (
+    [Parameter(Mandatory=$true)][string]$sourceSubscriptionId,
+    [Parameter(Mandatory=$true)][string]$sourceResourceGroupName,
+    [Parameter(Mandatory=$true)][string]$managedDiskName,
+    [Parameter(Mandatory=$true)][string]$targetResourceGroupName,
+    [Parameter(Mandatory=$true)][string]$targetSubscriptionId
+ )
+
+#Set the context to the subscription Id where Managed Disk exists
+Select-AzureRmSubscription -Subscription $sourceSubscriptionId
+ 
+#Get the source managed disk
+$managedDisk= Get-AzureRMDisk -ResourceGroupName $sourceResourceGroupName -DiskName $managedDiskName
+ 
+#Set the context to the subscription Id where managed disk will be copied to
+Select-AzureRmSubscription -Subscription $targetSubscriptionId
+ 
+$diskConfig=New-AzureRmDiskConfig -SourceResourceId $managedDisk.Id -Location $managedDisk.Location -CreateOption Copy
+ 
+#Create a new managed disk in the target subscription and resource group
+New-AzureRmDisk -Disk $diskConfig -DiskName $managedDiskName -ResourceGroupName $targetResourceGroupName
--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ### 01.VM_Migrate_region:
 ####
-Powershell script to migrate Azure VMs with managed disk across Geography
+copyVmAcrossRegion - Powershell script to migrate Azure VMs with managed disk across Geography
+####
+MigrateManagedVmDiskAcrossSubscription - Powershell script to migrate single managed Disk to different subscription in same geography
 
 ### 02.Subscription_Policies:
 ####
